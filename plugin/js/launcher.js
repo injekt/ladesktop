d41a94a2ee8a58484661f03dac3f8f908ebff0a8
liqiao02 <liqiao02@baidu.com>
2013-12-21 02:03:40 +0800

    add new api

--- plugin/js/launcher.js
+++ plugin/js/launcher.js
@@ -4,4 +4,7 @@ module.exports = {
   getNativeAppList : function(successCallback, errorCallback) {
     exec(successCallback, errorCallback, 'launcher', 'getNativeAppList', []);
   },
+  startApp : function(successCallback, errorCallback, packgeName) {
+    exec(successCallback, errorCallback, 'launcher', 'startApp', [packageName]);
+  },
 };
